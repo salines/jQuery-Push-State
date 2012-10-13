bd047a22773eac959a5e659266621f2c8eee43a7
Antonio Salvati <salvan13@gmail.com>
2012-10-13 09:55:49 +0200

    First Commit

---
+++ jquery.push.state.js
@@ -0,0 +1,141 @@
+/*!
+ * jQuery PushState plugin
+ * Original author: Antonio Salvati - @salvan13 - salvan13@gmail.com
+ * Licensed under the MIT license
+ */
+
+;(function ( $, window, document, undefined ) {
+
+    //defaults
+    var pluginName = 'pushState';
+    var defaults = {
+        debug: false,
+        load: [],
+        onBeforePageLoad: function(plugin, href) {
+            var containers = plugin.options.load;
+            for (var i = 0; i < containers.length; i++) {
+                var container = containers[i];
+                $(container).css({opacity: '0.4'});
+            }
+        },
+        onAfterPageLoad: function(plugin, href) {
+            var containers = plugin.options.load;
+            for (var i = 0; i < containers.length; i++) {
+                var container = containers[i];
+                $(container).css({opacity: '1'});
+            }
+        },
+        onFailPageLoad: function(plugin, href) {
+            alert("error while loading page \"" + href +"\"");
+        },
+        ignore: function(link, elem) {
+        	return (link && ( link.match("^http")=="http" || link.charAt(0) == "#" ));
+        }
+    };
+
+    // plugin constructor
+    function Plugin( element, options ) {
+        this.element = element;
+        this.options = $.extend( {}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    Plugin.prototype = {
+        
+        init: function () {
+            var self = this;
+
+            //apply pugin only if browser support history.pushState
+            if (!history.pushState) {
+                return;
+            }
+
+            $(self.element).on('click', 'a', function(e) {
+                var a = $(this);
+                var href = a.attr('href');
+                if(self.options.ignore(href, a)) {
+                    return true;
+                }
+                self._load(href, true);
+                return false;
+            });
+
+            var popped = ('state' in window.history) && window.history.state !== null, initialURL = location.href;
+            window.onpopstate = function(event) {
+              
+              // Ignore inital popstate that some browsers fire on page load
+              var initialPop = !popped && location.href == initialURL
+              popped = true
+              if ( initialPop ) return
+
+                if(event.state) {
+                    self._debug(event.state);
+                    self._load(event.state.href, false);
+                }else {
+                    location.reload();
+                }
+            }; 
+
+            
+        },
+
+        _load: function(href, push) {
+            var self = this;
+            self._debug("loading " + href);
+            self.options.onBeforePageLoad(self, href);
+            var containers = self.options.load;
+            $.ajax({
+                type: 'GET',
+                url: href
+            }).done(function(res) {
+            	
+            	var title = $(res).filter("title").text();
+            	$("title").text(title);
+            	
+                for (var i = 0; i < containers.length; i++) {
+                    var container = containers[i];
+                    var data = $(container, $(res)).contents(); 
+                    $(container).html(data);
+                }
+
+                if(push){
+                    var state = { href: href, title: title };
+                    history.pushState(state, title, href);
+                }
+
+            }).fail(function() {
+                self.options.onFailPageLoad(self, href);
+            }).always(function() {
+                self.options.onAfterPageLoad(self, href);
+            });
+        },
+
+        _debug: function(data) {
+            if(this.options.debug && window.console) {
+                console.log(data);
+            }
+        }
+
+    };
+
+    $.fn[pluginName] = function ( options ) {
+        var args = arguments;
+        if (options === undefined || typeof options === 'object') {
+            return this.each(function () {
+                if (!$.data(this, 'plugin_' + pluginName)) {
+                    $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+                }
+            });
+        } else if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
+            return this.each(function () {
+                var instance = $.data(this, 'plugin_' + pluginName);
+                if (instance instanceof Plugin && typeof instance[options] === 'function') {
+                    instance[options].apply( instance, Array.prototype.slice.call( args, 1 ) );
+                }
+            });
+        }
+    }
+
+})( jQuery, window, document );
